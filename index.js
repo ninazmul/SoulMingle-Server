eeb4535bb9b1a1c82b4091d41d5102ff47d514fe
ninazmul <nazmulsaw@gmail.com>
2023-12-04 21:09:59 +0600

    done

--- index.js
+++ index.js
@@ -12,12 +12,16 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
+      "http://localhost:5000/",
       "https://soul-mingle.web.app/",
       "https://soul-mingle-server.vercel.app/",
     ],
     credentials: true,
   })
 );
+
+const checkoutRequests = [];
+
 
 const uri =
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pc5txjd.mongodb.net/?retryWrites=true&w=majority`;
@@ -34,33 +38,35 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-      // await client.connect();
-      
+    // await client.connect();
+
     const bioDataCollection = client.db("BioDB").collection("BioData");
-    
+
     const favDataCollection = client.db("BioDB").collection("FavBio");
 
     const userCollection = client.db("BioDB").collection("UserData");
 
+    const checkoutCollection = client.db("BioDB").collection("CheckoutData");
 
     app.post("/jwt", async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
       res.send({ token });
     });
 
-
     app.post("/user", async (req, res) => {
       const user = req.body;
-      const query = { email: user.email }
+      const query = { email: user.email };
       const existingUser = await userCollection.findOne(query);
       if (existingUser) {
-        return res.send({ message: 'user already exists', insertedId: null });
+        return res.send({ message: "user already exists", insertedId: null });
       }
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
-    app.get("/user",  async (req, res) => {
+    app.get("/user", async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
@@ -91,25 +97,21 @@ async function run() {
       next();
     };
 
-    app.get(
-      "/user/admin/:email",
-      verifytoken,
-      async (req, res) => {
-        const email = req.params.email;
-        if (email !== req.decoded.email) {
-          return res.status(403).send({ message: "unauthorized access" });
-        }
-        const query = { email: email };
-        const user = await userCollection.findOne(query);
-        let admin = false;
-        if (user) {
-          admin = user?.role === "admin";
-        }
-        res.send({ admin });
+    app.get("/user/admin/:email", verifytoken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "unauthorized access" });
       }
-    );
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
+    });
 
-    app.get("/user/:email", async(req, res) => {
+    app.get("/user/:email", async (req, res) => {
       try {
         const email = req.params.email;
         const user = await userCollection.findOne({ email });
@@ -137,6 +139,34 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/checkout", async (req, res) => {
+      const createCheck = req.body;
+      const result = await checkoutCollection.insertOne(createCheck);
+      res.send(result);
+    });
+
+    app.get("/checkout", async (req, res) => {
+      const result = await checkoutCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.patch(
+      "/checkout/Approved/:id",
+      verifytoken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updatedDoc = {
+          $set: {
+            status: "Pending",
+          },
+        };
+        const result = await checkoutCollection.updateOne(filter, updatedDoc);
+        res.send(result);
+      }
+    );
+    
     app.post("/bioData", async (req, res) => {
       const createBio = req.body;
       const result = await bioDataCollection.insertOne(createBio);
@@ -182,11 +212,10 @@ async function run() {
       }
     });
 
-
-      app.get("/bioData", async (req, res) => {
-          const result = await bioDataCollection.find().toArray();
-          res.send(result);
-      });
+    app.get("/bioData", async (req, res) => {
+      const result = await bioDataCollection.find().toArray();
+      res.send(result);
+    });
     app.delete("/bioData/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -194,30 +223,47 @@ async function run() {
       res.send(result);
     });
 
-     app.get("/bioData/:id", async (req, res) => {
-       try {
-         const { id } = req.params;
-         const objectId = new ObjectId(id);
-         const bioData = await bioDataCollection.findOne({ _id: objectId });
+    app.get("/bioData/:id", async (req, res) => {
+      try {
+        const { id } = req.params;
+        const objectId = new ObjectId(id);
+        const bioData = await bioDataCollection.findOne({ _id: objectId });
 
-         if (!bioData) {
-           return res.status(404).json({ error: "Bio data not found" });
-         }
+        if (!bioData) {
+          return res.status(404).json({ error: "Bio data not found" });
+        }
 
-         res.json(bioData);
-       } catch (error) {
-         console.error("Error fetching bio details:", error);
-         res.status(500).json({ error: "Internal server error" });
-       }
-     });
-    
+        res.json(bioData);
+      } catch (error) {
+        console.error("Error fetching bio details:", error);
+        res.status(500).json({ error: "Internal server error" });
+      }
+    });
+
+    app.patch(
+      "/bioData/Premium/:id",
+      verifytoken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updatedDoc = {
+          $set: {
+            Subscription: "Premium",
+          },
+        };
+        const result = await bioDataCollection.updateOne(filter, updatedDoc);
+        res.send(result);
+      }
+    );
+
     app.get("/favBio", async (req, res) => {
       const email = req.query.email;
       const query = { email: email };
       const result = await favDataCollection.find(query).toArray();
       res.send(result);
-    })
-    
+    });
+
     app.post("/favBio", async (req, res) => {
       // Corrected parameter order
       const favBioCart = req.body;
@@ -232,12 +278,10 @@ async function run() {
 
     app.delete("/favBio/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
+      const query = { _id: new ObjectId(id) };
       const result = await favDataCollection.deleteOne(query);
       res.send(result);
-    })
-
-
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
